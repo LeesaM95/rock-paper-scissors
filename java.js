3d838beca3356487b9a8b07415e64cd77ef97406
LeesaM95 <146777629+LeesaM95@users.noreply.github.com>
2023-11-06 12:55:22 -0500

    added all folders and documents needed to complete assignment

---
+++ java.js
@@ -0,0 +1,22 @@
+var Win = 0;
+var Loss = 0;
+var Ties = 0;
+
+var Options = {
+    Rock: "R",
+    Paper: "P",
+    Scissors: "S",
+}
+
+// Get Users Choice
+ 
+
+// Get Computer's Choice
+
+
+// Compare choices to determine a winner
+
+
+
+// Start program and display results
+
